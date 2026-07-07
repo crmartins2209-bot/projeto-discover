fa6acedabcc1748a4995db267e7b3e179be027e6
Christofer Ramos Martins <christoferramosmartins@MacBook-Air-de-Christofer.local>
2026-07-06 21:08:27 -0300

    initial commit

---
+++ assets/script.js
@@ -0,0 +1,25 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  const img = document.querySelector("#profile img")
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./avatar-light.png")
+    img.setAttribute(
+      "alt",
+      "Foto de Mayk Brito sorrindo, usando oculos e camiseta preta",
+    )
+  } else {
+    img.setAttribute("src", "./avatar.png")
+    img.setAttribute(
+      "alt",
+      "Foto de Mayk Brito usando moletom preto, sorrindo e oculos escuros",
+    )
+  }
+
+  /* if(html.classList.contains("light")) {
+    html.classList.remove("light")
+  } else {
+    html.classList.add("light")
+  } */
+}
